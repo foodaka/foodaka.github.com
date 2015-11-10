251f5eb9de35aacbab04aebbf585ed1c1f5ab615
Mark Hinschberger <markhinschberger@gmail.com>
2015-11-10 15:03:29 +0800

    first commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,12 @@
+//= require_tree .
+//= require jquery  
+//= require bootstrap-sprockets
+@import "typed";
+
+$('.terminal').typewriting( "Hello World!", {
+
+  "typing_interval": 200,
+  "blink_interval": "1s",
+  "cursor_color": "#00fd55"
+}
+
